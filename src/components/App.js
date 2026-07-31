692846b3b367fd7c985d149ee12321f5f6ec48b3
Prateek7071 <tiwariprateek123@gmail.com>
2026-07-31 21:50:52 +0530

    1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Navigation from './Navigation';
 
-import React from "react";
-import './../styles/App.css';
-
-const App = () => {
+function App() {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+    <Router>
+      <div>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
